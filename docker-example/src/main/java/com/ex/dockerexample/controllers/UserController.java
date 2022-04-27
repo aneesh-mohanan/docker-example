549bfff27b22da18c90b5da2b148fff56e3d5aa3
aneesh-mohanan <aneeshcm182gmail.com>
2022-04-27 16:42:01 -0400

    initial commit

---
+++ docker-example/src/main/java/com/ex/dockerexample/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.ex.dockerexample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    public  String getMessage(){
+        return "Hello World";
+    }
+}
